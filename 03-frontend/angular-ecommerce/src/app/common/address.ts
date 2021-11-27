788d7394eee4bd53d6b2e1d41e15885fe096983c
andriig <a3060113@gmail.com>
2021-11-27 18:37:43 +0200

    lesson 24 Save the order - frontend

---
+++ 03-frontend/angular-ecommerce/src/app/common/address.ts
@@ -0,0 +1,10 @@
+export class Address {
+
+  street: string;
+  city: string;
+  this: string;
+  state: string;
+  country: string;
+  zipCode: string;
+
+}
--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -5,6 +5,11 @@ import {Country} from "../../common/country";
 import {State} from "../../common/state";
 import {Luv2ShopValidators} from "../../validators/luv2-shop-validators";
 import {CartService} from "../../services/cart.service";
+import {CheckoutService} from "../../services/checkout.service";
+import {Router} from "@angular/router";
+import {Order} from "../../common/order";
+import {OrderItem} from "../../common/order-item";
+import {Purchase} from "../../common/purchase";
 
 @Component({
   selector: 'app-checkout',
@@ -28,7 +33,10 @@ export class CheckoutComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
               private luv2ShopFormService: Luv2ShopFormService,
-              private cartService: CartService) {}
+              private cartService: CartService,
+              private checkoutService: CheckoutService,
+              private router: Router) {
+  }
 
   ngOnInit(): void {
 
@@ -37,17 +45,17 @@ export class CheckoutComponent implements OnInit {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: new FormControl('', [
-            Validators.required,
-            Validators.minLength(2),
-            Luv2ShopValidators.notOnlyWhitespace]),
+          Validators.required,
+          Validators.minLength(2),
+          Luv2ShopValidators.notOnlyWhitespace]),
         lastName: new FormControl('', [
-            Validators.required,
-            Validators.minLength(2),
-            Luv2ShopValidators.notOnlyWhitespace]),
+          Validators.required,
+          Validators.minLength(2),
+          Luv2ShopValidators.notOnlyWhitespace]),
         email: new FormControl('', [
-            Validators.required,
-            Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
-            Luv2ShopValidators.notOnlyWhitespace])
+          Validators.required,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+          Luv2ShopValidators.notOnlyWhitespace])
       }),
       shippingAddress: this.formBuilder.group({
         street: [],
@@ -109,7 +117,60 @@ export class CheckoutComponent implements OnInit {
 
     if (this.checkoutFormGroup.invalid) {
       this.checkoutFormGroup.markAllAsTouched();
+      return;
     }
+
+    // set up order
+    let order = new Order();
+    order.totalPrice = this.totalPrice;
+    order.totalQuantity = this.totalQuantity;
+
+    // get cart items
+    const cartItems = this.cartService.cartItems;
+
+    // create order items from cart items
+    let orderItems: OrderItem[] = cartItems.map(item => new OrderItem(item));
+
+    // set up purchase
+    let purchase: Purchase = new Purchase();
+
+    // populate purchase - customer
+    purchase.customer = this.checkoutFormGroup.controls['customer'].value;
+
+    // populate purchase - shipping address
+    purchase.shippingAddress = this.checkoutFormGroup.controls['shippingAddress'].value;
+    const shippingState: State = JSON.parse(JSON.stringify(purchase.shippingAddress.state));
+    const shippingCountry: Country = JSON.parse(JSON.stringify(purchase.shippingAddress.country));
+    purchase.shippingAddress.state = shippingState.name;
+    purchase.shippingAddress.country = shippingCountry.name;
+
+    // populate purchase - billing address
+    purchase.billingAddress = this.checkoutFormGroup.controls['billingAddress'].value;
+    const billingState: State = JSON.parse(JSON.stringify(purchase.billingAddress.state));
+    const billingCountry: Country = JSON.parse(JSON.stringify(purchase.billingAddress.country));
+    purchase.billingAddress.state = billingState.name;
+    purchase.billingAddress.country = billingCountry.name;
+
+    // populate purchase - order and orderItems
+
+    purchase.order = order;
+    purchase.orderItems = orderItems;
+
+    // post REST API via checkout service
+
+    this.checkoutService.placeOrder(purchase).subscribe({
+        next: response => {
+          alert(`Your order has been received. \nOrder tracking number: ${response.orderTrackingNumber}`)
+
+          // reset card
+          this.resetCart()
+
+        },
+        error: err => {
+          alert(`There was an error: ${err.message}`)
+        }
+      }
+    );
 
   }
 
@@ -200,5 +261,17 @@ export class CheckoutComponent implements OnInit {
     this.cartService.totalPrice.subscribe(
       totalPrice => this.totalPrice = totalPrice
     )
+  }
+
+  private resetCart() {
+    // reset cart data
+    this.cartService.cartItems = [];
+    this.cartService.totalPrice.next(0);
+    //reset the form
+    this.checkoutFormGroup.reset();
+
+    // navigate to main page
+    this.router.navigateByUrl("/products");
+
   }
 }
