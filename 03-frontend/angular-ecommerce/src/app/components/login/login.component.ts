23d92c6b7d5c31032ce11f3e070a4fa1b5df3de1
andriig <a3060113@gmail.com>
2021-11-28 18:03:59 +0200

    lesson 25 login form

---
+++ 03-frontend/angular-ecommerce/src/app/components/login/login.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit} from '@angular/core';
+import * as OktaSignIn from '@okta/okta-signin-widget'
+
+import myAppConfig from '../../config/my-app-config';
+import {OktaAuth} from "@okta/okta-auth-js";
+import {OktaAuthStateService} from "@okta/okta-angular";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  oktaSignIn: any;
+
+  constructor(private oktaAuth: OktaAuth,
+              private oktaAuthStateService: OktaAuthStateService) {
+    this.oktaSignIn = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.oktaSignIn.remove();
+    this.oktaSignIn.renderEl({
+        el: '#okta-sign-in-widget'
+      },
+      (response) => {
+        if (response.status === 'SUCCESS') {
+          this.oktaAuth.signInWithRedirect();
+        }
+      },
+      (error) => {
+        throw error;
+      }
+    );
+  }
+
+}
---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+  oidc: {
+    clientId: '0oa2w5qtzqYU1iHLi5d7',
+    issuer: 'https://dev-28051690.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+
+}
